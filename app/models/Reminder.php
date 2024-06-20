234e09feea3999f452a3f5ff472e2adef9b43751
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-20 02:58:09 +0000

    Create files for reminder functionality
    Create reminders view, model and controller files

---
+++ app/models/Reminder.php
@@ -0,0 +1,23 @@
+<?php
+
+class Reminder {
+
+
+    public function __construct() {
+
+    }
+
+    public function get_all_reminders () {
+      $db = db_connect();
+      $statement = $db->prepare("select * from reminders;");
+      $statement->execute();
+      $rows = $statement->fetch(PDO::FETCH_ASSOC);
+      return $rows;
+    }
+
+    public function update_reminder ($reminder_id) {
+     //TODO: Update Statement 
+    }
+}
+
+?>
---
+++ app/views/reminders/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- <link rel="stylesheet" href="/app/views/css/login.css"> -->
+</head>
+
+<main role="main" class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Reminders</h1>
+            </div>
+        </div>
+    </div>
