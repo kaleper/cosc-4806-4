1da7b0396a0327d23a8ca58df0055d58fa08c004
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-26 19:58:31 +0000

    Format navbar
    Moved navbar to template folder. Required in home and reminders views. Removed set-up bootstrap code. Added working hyperlinks.
    
    Moved logout to navbar, removed from body

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -1,34 +1,35 @@
-<?php require_once 'app/views/templates/header.php' ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <!-- <link rel="stylesheet" href="/app/views/css/login.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="app/views/css/reminders.css"> 
 </head>
 
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
+<body>
+    <?php require_once 'app/views/templates/navbar.php' ?>
+
+    <div class="container main">
+        <div class="row mt-4">
             <div class="col-lg-12">
-                <h1>Reminders</h1>
-                <p> <a href="/reminders/create">Create a new reminder</a></p>
+                <h3 class="reminders">Reminders</h3>
+                <p> <a class="link" href="/reminders/create">Create a new reminder</a></p>
             </div>
         </div>
     </div>
-    <?php
+</body>
 
-    // print_r($data['reminders']);
+<!-- 
+    print_r($data['reminders']);
+        Code below, doesn't display reminder subject . . . $data -> why is it undefined?
+        
+        foreach ($data['reminders'] as $reminder) {
+        echo "<p>" . $reminder['subject'] . ' <a href="/reminder/update">update</a> delete</p>';
 
-
-        // Code below, doesn't display reminder subject . . . $data -> why is it undefined?
+} -->
     
-        // foreach ($data['reminders'] as $reminder) {
-        //     echo "<p>" . $reminder['subject'] . ' <a href="/reminder/update">update</a> delete</p>';
-
-        // }
-    ?>
 <!-- TODO: Display in table format by iterating through reminders -->
-<?php require_once 'app/views/templates/footer.php' ?>
+
 
