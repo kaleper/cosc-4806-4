553e0ba6c43166d8ede7e5d634cf35b80aa45e02
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-28 18:14:07 +0000

    Change table name to 'notes' from 'reminders'

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -10,7 +10,7 @@ class Reminder {
     public function get_all_reminders () {
       $db = db_connect();
       // Selects reminders only associated with logged in user
-      $statement = $db->prepare("SELECT * from reminders WHERE user_id = :userid");
+      $statement = $db->prepare("SELECT * from notes WHERE user_id = :userid");
       $statement->bindParam(':userid', $_SESSION['userid']);
       $statement->execute();
       $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -21,7 +21,7 @@ class Reminder {
        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           $db = db_connect();
           // var_dump($_SESSION['userid']);
-          $statement = $db->prepare("INSERT INTO reminders(subject, user_id) VALUES (:subject, :userid)");
+          $statement = $db->prepare("INSERT INTO notes(subject, user_id) VALUES (:subject, :userid)");
           $statement->bindParam(':subject', $new_reminder);
           $statement->bindParam(':userid', $_SESSION['userid']);
           $statement->execute();
@@ -36,7 +36,7 @@ class Reminder {
         // Used to prepopulate reminder update form with existing reminder info
       public function get_reminder_by_id($reminder_id) {
            $db = db_connect();
-           $statement = $db->prepare("SELECT * FROM reminders WHERE id = :id");
+           $statement = $db->prepare("SELECT * FROM notes WHERE id = :id");
            $statement->bindParam(':id', $reminder_id);
            $statement->execute();
            return $statement->fetch(PDO::FETCH_ASSOC);
@@ -45,7 +45,7 @@ class Reminder {
         // Triggered when user updates reminder (submits form on update_reminder.php)
       public function update_reminder_text($reminder_id, $update_reminder_text) {
           $db = db_connect();
-          $statement = $db->prepare("UPDATE reminders SET subject = :subject WHERE id = :id");
+          $statement = $db->prepare("UPDATE notes SET subject = :subject WHERE id = :id");
           $statement->bindParam(':subject', $update_reminder_text);
           $statement->bindParam(':id', $reminder_id);
           return $statement->execute();
@@ -54,21 +54,21 @@ class Reminder {
 
      public function delete_reminder($reminder_id) {
           $db = db_connect();
-          $statement = $db->prepare("DELETE FROM reminders WHERE id = :id");
+          $statement = $db->prepare("DELETE FROM notes WHERE id = :id");
           $statement->bindParam(':id', $reminder_id);
           return $statement->execute();
       }
 
     public function complete_reminder($reminder_id) {
           $db = db_connect();
-          $statement = $db->prepare("UPDATE reminders SET completed = true WHERE id = :id");
+          $statement = $db->prepare("UPDATE notes SET completed = true WHERE id = :id");
           $statement->bindParam(':id', $reminder_id);
           return $statement->execute();
       }
 
     public function uncomplete_reminder($reminder_id) {
           $db = db_connect();
-          $statement = $db->prepare("UPDATE reminders SET completed = false WHERE id = :id");
+          $statement = $db->prepare("UPDATE notes SET completed = false WHERE id = :id");
           $statement->bindParam(':id', $reminder_id);
           return $statement->execute();
       }
